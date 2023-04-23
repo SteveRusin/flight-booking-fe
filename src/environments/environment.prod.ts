a228ecd373918e875ef2322c2c4db9e78523eaab
SteveRusin <steve.neeson21@gmail.com>
2023-04-23 14:21:35 +0300

    update be url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 import { Environment } from './environment.models';
 
 export const environment: Environment = {
-  routesApiUrl:
-    'http://flightbookingalb-1400347209.eu-west-1.elb.amazonaws.com',
+  routesApiUrl: 'http://flightBookingAlb-411190737.eu-west-1.elb.amazonaws.com',
 };
